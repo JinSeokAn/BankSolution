0fd4faf12f51cb1e8d2a328d0445463c44950b72
JinSeokAn <dkswlstjr000@naver.com>
2023-12-22 17:53:10 +0900

    Setting : Project Setting

---
+++ src/main/java/com/banksolusion/BankSolusionApplication.java
@@ -0,0 +1,13 @@
+package com.banksolusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSolusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSolusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banksolusion/BankSolusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksolusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSolusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
